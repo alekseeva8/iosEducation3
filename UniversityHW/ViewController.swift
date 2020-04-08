da224a973ced1b3c07e9cc858325fbd47e026f9b
alekseeva8 <alekseeva.ev8@gmail.com>
2020-04-08 12:58:11 +0300

    Initial Commit

---
+++ UniversityHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniversityHW
+//
+//  Created by Elena Alekseeva on 4/8/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
