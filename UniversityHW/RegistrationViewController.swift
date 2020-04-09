1cfc0b4b5e3f452e082feffa82b2de4f998596b2
alekseeva8 <alekseeva.ev8@gmail.com>
2020-04-09 18:46:34 +0300

    HW done (variant 1) fixed!

---
+++ UniversityHW/RegistrationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationViewController.swift
+//  UniversityHW
+//
+//  Created by Elena Alekseeva on 4/9/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
