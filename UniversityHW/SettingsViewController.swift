0d3e8775e0b5438e42353a3abefa1a857e9d59c7
alekseeva8 <alekseeva.ev8@gmail.com>
2020-04-20 14:18:47 +0300

    SettingsVC

---
+++ UniversityHW/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  UniversityHW
+//
+//  Created by Elena Alekseeva on 4/20/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var segmentedControll: UISegmentedControl!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(named: "BackgroundColor")
+    }
+
+    @IBAction func segmentedControlPressed(_ sender: Any) {
+        let segmentIndex = segmentedControll.selectedSegmentIndex
+        switch segmentIndex {
+        case 0:
+            self.parent?.overrideUserInterfaceStyle = .unspecified
+        case 1:
+        self.parent?.overrideUserInterfaceStyle = .light
+        case 2:
+            self.parent?.overrideUserInterfaceStyle = .dark
+        default:
+            break
+        }
+    }
+}
